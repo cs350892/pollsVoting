e28bdb131c2ba577e5a99133785c2464322112e3
ChandraShekhar <104549787+cs350892@users.noreply.github.com>
2025-10-31 01:06:00 -0700

    Change API base URL to local development server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://pollsvoting-backend.onrender.com' });
+const API = axios.create({ 
+  baseURL: 'http://localhost:5000/api'  
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
